03755c475e0c5cecd696f4035bec5b416291050f
Alex Shevelkin <ash@tvip.ru>
2021-01-11 14:58:55 +0300

    Update jethub-j100 -uboot: jethome_armbian_jXXX_v1: move partition layout to separate variable -uboot: jethome_armbian_jXXX_v1: write chipid to memory address specified in chipid 2nd parameter

--- jethub-j100/timestamp_autogenerated.h
+++ jethub-j100/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Jan 11 2021"
-#define U_BOOT_TIME "10:57:54"
+#define U_BOOT_TIME "14:47:52"
--- jethub-j100/version_autogenerated.h
+++ jethub-j100/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2015.01-armbian+"
+#define PLAIN_VERSION "2015.01-armbian-g2b843aec01"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CONFIG_SYSTEM_AS_ROOT ""
 #define CONFIG_AVB2 ""
