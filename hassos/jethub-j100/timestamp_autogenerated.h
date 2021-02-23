0d6541205ca3ebeec581049c1c20c6878330cdcf
Vyacheslav Bocharov <devel@lexina.in>
2021-02-23 18:40:38 +0300

    Fix hassos

--- hassos/jethub-j100/timestamp_autogenerated.h
+++ hassos/jethub-j100/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
 #define U_BOOT_DATE "Feb 23 2021"
-#define U_BOOT_TIME "12:21:57"
+#define U_BOOT_TIME "18:40:21"
--- hassos/jethub-j80/version_autogenerated.h
+++ hassos/jethub-j80/version_autogenerated.h
@@ -1,4 +1,4 @@
-#define PLAIN_VERSION "2015.01-g390e67b"
+#define PLAIN_VERSION "2015.01-ge1ce3aa"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CONFIG_SYSTEM_AS_ROOT ""
 #define CONFIG_AVB2 ""
