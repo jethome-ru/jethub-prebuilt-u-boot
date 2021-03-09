655c22258a4b6a8c9cb1c50f4a464f1551fd4981
Vyacheslav Bocharov <devel@lexina.in>
2021-03-09 11:39:17 +0300

    Hassos: Test eth phy reset in u-boot for j100

--- hassos/jethub-j100/timestamp_autogenerated.h
+++ hassos/jethub-j100/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
-#define U_BOOT_DATE "Mar 02 2021"
-#define U_BOOT_TIME "11:15:09"
+#define U_BOOT_DATE "Mar 09 2021"
+#define U_BOOT_TIME "11:28:48"
--- hassos/jethub-j80/timestamp_autogenerated.h
+++ hassos/jethub-j80/timestamp_autogenerated.h
@@ -1,2 +1,2 @@
-#define U_BOOT_DATE "Mar 02 2021"
-#define U_BOOT_TIME "11:14:54"
+#define U_BOOT_DATE "Mar 09 2021"
+#define U_BOOT_TIME "11:28:34"
