8015a5cf8e0cef9eb7d305f063e522e85d7c47bf
Vyacheslav Bocharov <devel@lexina.in>
2021-04-07 13:42:15 +0300

    Add testing u-boot 2021.04 for J100

--- jethub-j100/timestamp_autogenerated.h
+++ jethub-j100/timestamp_autogenerated.h
@@ -1,2 +1,6 @@
-#define U_BOOT_DATE "Mar 01 2021"
-#define U_BOOT_TIME "13:16:18"
+#define U_BOOT_DATE "Apr 07 2021"
+#define U_BOOT_TIME "13:29:31"
+#define U_BOOT_TZ "+0300"
+#define U_BOOT_DMI_DATE "04/07/2021"
+#define U_BOOT_BUILD_DATE 0x20210407
+#define U_BOOT_EPOCH 1617791371
--- jethub-j100/version_autogenerated.h
+++ jethub-j100/version_autogenerated.h
@@ -1,6 +1,6 @@
-#define PLAIN_VERSION "2015.01-armbian-g4a4820487c"
+#define PLAIN_VERSION "2021.04-00070-ge665562802"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define CONFIG_SYSTEM_AS_ROOT ""
-#define CONFIG_AVB2 ""
-#define CC_VERSION_STRING "aarch64-none-elf-gcc (crosstool-NG linaro-1.13.1-4.8-2013.11 - Linaro GCC 2013.10) 4.8.3 20131111 (prerelease)"
-#define LD_VERSION_STRING "GNU ld (crosstool-NG linaro-1.13.1-4.8-2013.11 - Linaro GCC 2013.10) 2.23.2.20130610 Linaro 2013.10-4"
+#define U_BOOT_VERSION_NUM 2021
+#define U_BOOT_VERSION_NUM_PATCH 4
+#define CC_VERSION_STRING "aarch64-elf-gcc (Linaro GCC 7.5-2019.12) 7.5.0"
+#define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2019.12) 2.28.2.20170706"
