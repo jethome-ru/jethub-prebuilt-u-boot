31d44063f6b0da5fa0c4b027a6b298f144e53d95
Vyacheslav Bocharov <devel@lexina.in>
2021-04-29 14:11:00 +0300

    j100: new image by linaro 7.5 gcc

--- jethub-j100/version_autogenerated.h
+++ jethub-j100/version_autogenerated.h
@@ -1,8 +1,8 @@
-#define PLAIN_VERSION "2019.01-gf3dccc73f72-dirty"
-#define ACS_VERSION "axg_jethubj100_v1-gf3dccc73f72-dirty"
+#define PLAIN_VERSION "2019.01-g7a8592f12f3"
+#define ACS_VERSION "axg_jethubj100_v1-g7a8592f12f3"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
 #define CONFIG_SYSTEM_AS_ROOT ""
 #define CONFIG_AVB2 ""
 #define CONFIG_CMD_BOOTCTOL_VAB ""
-#define CC_VERSION_STRING "aarch64-none-elf-gcc (GNU Toolchain for the A-profile Architecture 10.2-2020.11 (arm-10.16)) 10.2.1 20201103"
-#define LD_VERSION_STRING "GNU ld (GNU Toolchain for the A-profile Architecture 10.2-2020.11 (arm-10.16)) 2.35.1.20201028"
+#define CC_VERSION_STRING "aarch64-elf-gcc (Linaro GCC 7.5-2019.12) 7.5.0"
+#define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2019.12) 2.28.2.20170706"
