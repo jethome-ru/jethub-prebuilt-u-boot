fb18627e8e5a0c9dee91a514b7d78673be818387
Vyacheslav Bocharov <devel@lexina.in>
2021-05-12 20:19:45 +0300

    mainline uboot test j80

--- jethub-j80/version_autogenerated.h
+++ jethub-j80/version_autogenerated.h
@@ -1,8 +1,6 @@
-#define PLAIN_VERSION "2019.01-g1d6461b0bde-dirty"
-#define ACS_VERSION "gxl_jethubj80_v1-g1d6461b0bde-dirty"
+#define PLAIN_VERSION "2021.07-rc2-00050-g2b3e62cc1d8"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define CONFIG_SYSTEM_AS_ROOT ""
-#define CONFIG_AVB2 ""
-#define CONFIG_CMD_BOOTCTOL_VAB ""
+#define U_BOOT_VERSION_NUM 2021
+#define U_BOOT_VERSION_NUM_PATCH 7
 #define CC_VERSION_STRING "aarch64-elf-gcc (Linaro GCC 7.5-2019.12) 7.5.0"
 #define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2019.12) 2.28.2.20170706"
