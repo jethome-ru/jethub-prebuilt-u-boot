689be92f9edfbbff150864eb1e4f54ffa5541ddf
Vyacheslav Bocharov <devel@lexina.in>
2021-05-19 13:30:01 +0300

    Update for v2021.07-rc2 with amlogic patches

--- jethub-j100/version_autogenerated.h
+++ jethub-j100/version_autogenerated.h
@@ -1,8 +1,6 @@
-#define PLAIN_VERSION "2019.01-g7a8592f12f3"
-#define ACS_VERSION "axg_jethubj100_v1-g7a8592f12f3"
+#define PLAIN_VERSION "2021.07-rc2-00079-g90a40130def"
 #define U_BOOT_VERSION "U-Boot " PLAIN_VERSION
-#define CONFIG_SYSTEM_AS_ROOT ""
-#define CONFIG_AVB2 ""
-#define CONFIG_CMD_BOOTCTOL_VAB ""
+#define U_BOOT_VERSION_NUM 2021
+#define U_BOOT_VERSION_NUM_PATCH 7
 #define CC_VERSION_STRING "aarch64-elf-gcc (Linaro GCC 7.5-2019.12) 7.5.0"
 #define LD_VERSION_STRING "GNU ld (Linaro_Binutils-2019.12) 2.28.2.20170706"
